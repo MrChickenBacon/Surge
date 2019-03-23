755ef3a382768eb14bf17a06db4c4c108fb5a69e
MrChickenBacon <34237142+MrChickenBacon@users.noreply.github.com>
2019-03-23 12:50:00 +0100

    Some stuff

---
+++ larvik-vær/src/components/WeatherBlock.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Her skal det komme data ja!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeatherBlock",
+  mounted() {
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
